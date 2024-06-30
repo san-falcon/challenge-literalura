767df55957f3e3f634a45de9af85b101d8537fe5
san-falcon <sanfalcon200@gmail.com>
2024-06-30 09:48:03 -0500

    Modificaciones
    - Se implemento en metodo run
    - Se agrego la dependencia jackson
    - Se configuro la conexion a la base de datos

--- src/main/java/com/sanfalcon/literalura/LiteraluraApplication.java
+++ src/main/java/com/sanfalcon/literalura/LiteraluraApplication.java
@@ -1,13 +1,32 @@
 package com.sanfalcon.literalura;
 
+import com.sanfalcon.literalura.principal.Principal;
+import com.sanfalcon.literalura.repository.AutorRepository;
+import com.sanfalcon.literalura.repository.IdiomaRepository;
+import com.sanfalcon.literalura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
-	}
+    @Autowired
+    private LibroRepository libroRepository;
+    @Autowired
+    private AutorRepository autorRepository;
+    @Autowired
+    private IdiomaRepository idiomaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //? Pruebas con el consumo de api usando los servicios creados
+        Principal principal = new Principal(this.libroRepository, this.autorRepository, this.idiomaRepository);
+        principal.muestraElMenu();
+    }
 }
