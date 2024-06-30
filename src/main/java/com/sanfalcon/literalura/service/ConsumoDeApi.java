e6284bad003fe08d3b7faf8f4bb6a19b358092e9
san-falcon <sanfalcon200@gmail.com>
2024-06-30 09:40:03 -0500

    Creacion de los servicios
    - Servicios de consumo de api

---
+++ src/main/java/com/sanfalcon/literalura/service/ConsumoDeApi.java
@@ -0,0 +1,27 @@
+package com.sanfalcon.literalura.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoDeApi {
+    public String obtenerDatos(String url) {
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
